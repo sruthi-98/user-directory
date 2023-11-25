92f65cadd1a12a5e1d577066e0400e9769c8d5fd
Sruthi Suresh <sruthisuresh321@gmail.com>
2023-11-26 00:07:06 +0530

    Update user card styles

--- src/pages/Directory/components/UserCard.tsx
+++ src/pages/Directory/components/UserCard.tsx
@@ -8,7 +8,7 @@ interface UserCardProps {
 export const UserCard = ({ posts, user }: UserCardProps) => {
   return (
     <Link to={`/user/${user?.id}`}>
-      <aside className="flex justify-between p-5 border-2 rounded-[5px]">
+      <aside className="flex flex-col sm:flex-row gap-2 justify-between p-5 border-2 rounded-[5px]">
         <p>
           <b>Name:</b> {user?.name}
         </p>
